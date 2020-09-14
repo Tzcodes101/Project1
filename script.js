423752b196dca5cf0206491b76b0d93b5a5039c6
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-09-14 05:22:15 -0400

    working on image API, issues with CORS

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-//image container
-var imgKey = 'xaS4dqp03gmssoN5IOx7k3uIxZP08AYm8E9ab86QY3c';
-var queryURL = 'https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=1080&fit=max';
+$(document).ready(function(){
 
-function getImage(){
-    $ajax({
-        url: queryURL,
+//IMAGE CONTAINER
+
+var apiKey = '2XOC8Ho3yFg21bZa4yEJsklnaWbW1mg0';
+var queryUrl = 'http://wallhaven.cc/api/v1/search?apikey=' + apiKey;
+
+$('button').click(function(){
+    $.ajax({
+        url: queryUrl,
         type: 'GET',
-        dataType: 'json',
         success: function(data){
-           var imgTag =  $('<img>').attr('src', queryURL);
-           $('image').append(imgTag);
+          var imgEl =  $('<img>').attr('src', queryUrl);
+          $('#image').append(imgEl);
         }
     })
-}
-getImage();
+})
 
 
 
+//MUSIC CONTAINER 
 
 
 
-
-
-//music container 
-
-
-
-//random quotes
+//RANDOM INSPIRATIONAL QUOTES CONTAINER
 //http://paperquotes.com/
-$.ajax({ 
-    type : "GET", 
-    url : "https://api.paperquotes.com/apiv1/quotes/", 
-    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Token {token}');},
-    success : function(result) { 
-        $( "#quote-button" ).click(function() {
-            $('random-quote').text(result);
-          });
+// $.ajax({ 
+//     type : "GET", 
+//     url : "https://api.paperquotes.com/apiv1/quotes/", 
+//     beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Token {token}');},
+//     success : function(result) { 
+//         $( "#quote-button" ).click(function() {
+//             $('random-quote').text(result);
+//           });
         
-    }, 
-    error : function(result) { 
-      //handle the error
-    } 
-  }); 
+//     }, 
+//     error : function(result) { 
+//       //handle the error
+//     } 
+//   }); 
+
+
+})
