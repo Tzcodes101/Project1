0313427ca0e26925a6e1e05ec097702bce1020a3
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-09-14 00:26:29 -0400

    fixed typo in style.css and writing ajax for image container. Code still not working.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 //image container
 var imgKey = 'xaS4dqp03gmssoN5IOx7k3uIxZP08AYm8E9ab86QY3c';
-var queryURL = 'https://unsplash.com/documentation#search-photos';
+var queryURL = 'https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=1080&fit=max';
+
+function getImage(){
+    $ajax({
+        url: queryURL,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+           var imgTag =  $('<img>').attr('src', queryURL);
+           $('image').append(imgTag);
+        }
+    })
+}
+getImage();
+
+
+
+
+
+
 
 
 //music container 
