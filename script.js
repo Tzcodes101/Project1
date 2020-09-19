b35aacfa3071990d62921e3d0ae9b2460e2d5259
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-09-19 10:06:06 -0400

    js edits

--- script.js
+++ script.js
@@ -1,117 +1,56 @@
-<<<<<<< HEAD
-  
-$(document).ready(function(){
+$(document).ready(function () {
 
     //IMAGE CONTAINER
-    
-    $('#get-image').click(function(){
+
+    $('#get-image').click(function () {
         var userInput = $('#user-input').val();
         var apiKey = '18310078-72a2f7e072a362d9cdc81c6c9';
         var queryUrl = 'https://pixabay.com/api/?key=' + apiKey + '&q=' + userInput + '&image_type=photo';
-    
-    
-    
+
+
+
         $.ajax({
             url: queryUrl,
             type: 'GET',
-        }).then(function(data){
-            console.log(data);
+        }).then(function (data) {
             $('#image').empty();
             var imgLink = data.hits[4].largeImageURL;
-            console.log(imgLink);
-            var imgTag =  $('<img>').attr('src', imgLink).addClass('img-fluid');
+            var imgTag = $('<img>').attr('src', imgLink).addClass('img-fluid');
             var imgDisplay = $('#image').append(imgTag);
-            
         })
-    
-    });
 
-        
-
-//paperquotes, method as get or post not recognized
-// $.ajax({
-//     url: "https://cors-anywhere.herokuapp.com/https://api.paperquotes.com/apiv1/quotes/",
-//     method = "POST",
-//     type = "json",
-// }).then(function(data) {
-//     console.log(data);
-// })
-    
-
-    console.log("hello");
-    $.ajax({
-        url: "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0",
-        data: {
-            method: 'getQuote',
-            lang: "en",
-            format: "json"
-        },
-        success: function(response) {
-            console.log(response.quoteText);
-
-        }
-    
-    });
-
-});
-=======
-$(document).ready(function(){
-
-//IMAGE CONTAINER
-
-$('#get-image').click(function(){
-    var userInput = $('#user-input').val();
-    var apiKey = '18310078-72a2f7e072a362d9cdc81c6c9';
-    var queryUrl = 'https://pixabay.com/api/?key=' + apiKey + '&q=' + userInput + '&image_type=photo';
-
-
-
-    $.ajax({
-        url: queryUrl,
-        type: 'GET',
-    }).then(function(data){
-        $('#image').empty();
-        var imgLink = data.hits[4].largeImageURL;
-        var imgTag =  $('<img>').attr('src', imgLink).addClass('img-fluid');
-        var imgDisplay = $('#image').append(imgTag);
     })
 
-})
 
+    //RANDOM INSPIRATIONAL QUOTES CONTAINER
+    //add on-click event to get quote button id quote-button
 
+    $("#quote-button").click(function () {
 
-//MUSIC CONTAINER 
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?",
+            data: {
+                method: 'getQuote',
+                lang: "en",
+                format: "json"
+            },
+            success: function (response) {
+                $("#random-quote").empty();
+                //grab quote text and author
+                var quoteText = response.quoteText;
+                var quoteAuthor = response.quoteAuthor;
 
-// https://binaryjazz.us/genrenator-api/
-// https://developer.kkbox.com/
-// https://www.mixcloud.com/developers/
-// https://www.theaudiodb.com/api_guide.php
+                //create elements for vars
+                var displayQuoteEl = $("<h5>").text(quoteText);
+                var displayAuthorEl = $("<p>").text(quoteAuthor);
 
+                //append to container
+                $("#random-quote").append(displayQuoteEl).append(displayAuthorEl);
 
+            }
 
-//RANDOM INSPIRATIONAL QUOTES CONTAINER
-
-// maybe this one too : https://api.adviceslip.com/
-// another one: https://favqs.com/api
-//https://forismatic.com/en/api/
-// https://quotesondesign.com/api/
-
-//http://paperquotes.com/
-// $.ajax({ 
-//     type : "GET", 
-//     url : "https://api.paperquotes.com/apiv1/quotes/", 
-//     beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Token {token}');},
-//     success : function(result) { 
-//         $( "#quote-button" ).click(function() {
-//             $('random-quote').text(result);
-//           });
-        
-//     }, 
-//     error : function(result) { 
-//       //handle the error
-//     } 
-//   }); 
+        });
+    });
 
 
 })
->>>>>>> css-edits
